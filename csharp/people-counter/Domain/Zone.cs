7f461fa849f9aeb917a554db090dcb794bca4388
Thomas Coopman <thomas.coopman@gmail.com>
2021-08-24 19:55:35 +0200

    refactor: rename counter to cameras

--- csharp/people-counter/Domain/Zone.cs
+++ csharp/people-counter/Domain/Zone.cs
@@ -6,20 +6,20 @@ namespace Domain
     public class Zone
     {
         private readonly string _name;
-        private readonly List<Camera> _counters;
+        private readonly List<Camera> _cameras;
         private CurrentCount _currentCount;
         public int Occupancy => _currentCount.Total;
 
-        public Zone(string name, List<Camera> counters)
+        public Zone(string name, List<Camera> cameras)
         {
             _name = name;
-            _counters = counters;
+            _cameras = cameras;
             _currentCount = new CurrentCount(0, 0);
         }
 
         public void Update()
         {
-            this._currentCount = this._counters.Aggregate(new CurrentCount(0, 0),
+            this._currentCount = this._cameras.Aggregate(new CurrentCount(0, 0),
                 ((count, camera) => count.Add(camera.CurrentCount())));
         }
     }
