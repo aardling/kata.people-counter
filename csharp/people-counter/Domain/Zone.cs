3f6e89e03ec8c1533d8ed3bf5cfc8c4ea770ff8e
Thomas Coopman <thomas.coopman@gmail.com>
2021-08-25 20:20:30 +0200

    refactor: remove unused function

--- csharp/people-counter/Domain/Zone.cs
+++ csharp/people-counter/Domain/Zone.cs
@@ -2,8 +2,5 @@ using System.Collections.Generic;
 
 namespace Domain
 {
-    public record Zone(string Name, List<Camera> Cameras)
-    {
-        public static int Occupancy(CameraMeasurement cameraMeasurement) => cameraMeasurement.Total;
-    }
+    public record Zone(string Name, List<Camera> Cameras);
 }
