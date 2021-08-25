b2ec5ddaea894976735ef93bc5021f8d0184546a
Thomas Coopman <thomas.coopman@gmail.com>
2021-08-25 15:46:49 +0200

    refactor: rename use domain language people in/out

--- csharp/people-counter/Domain/CameraMeasurement.cs
+++ csharp/people-counter/Domain/CameraMeasurement.cs
@@ -3,14 +3,14 @@ using System.Linq;
 
 namespace Domain
 {
-    public record CameraMeasurement(int InAmount, int OutAmount)
+    public record CameraMeasurement(int PeopleIn, int PeopleOut)
     {
         public CameraMeasurement Add(CameraMeasurement other) =>
-            new CameraMeasurement(this.InAmount + other.InAmount, this.OutAmount + other.OutAmount);
+            new CameraMeasurement(this.PeopleIn + other.PeopleIn, this.PeopleOut + other.PeopleOut);
 
         public static CameraMeasurement Sum(IEnumerable<CameraMeasurement> all) =>
             all.Aggregate(new CameraMeasurement(0, 0), (sum, other) => sum.Add(other));
 
-        public int Total => InAmount - OutAmount;
+        public int Total => PeopleIn - PeopleOut;
     }
 }
