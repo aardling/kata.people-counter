dc76273c93a9b0bb71677e4ba6d5b060ebfe87e0
Thomas Coopman <thomas.coopman@gmail.com>
2021-08-25 15:52:29 +0200

    refactor: rename use domain language

--- csharp/people-counter/Domain.Tests/CameraMeasurementTest.cs
+++ csharp/people-counter/Domain.Tests/CameraMeasurementTest.cs
@@ -2,10 +2,10 @@ using Xunit;
 
 namespace Domain.Tests
 {
-    public class CurrentCountTest
+    public class CameraMeasurementTest
     {
         [Fact]
-        public void ShouldAddTwoCurrentCounts()
+        public void ShouldAddTwoMeasurements()
         {
             var actual = new CameraMeasurement(1, 1).Add(new CameraMeasurement(4, 1));
             Assert.Equal(new CameraMeasurement(5, 2).Total, actual.Total);
