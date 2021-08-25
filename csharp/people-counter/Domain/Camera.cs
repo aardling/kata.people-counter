9599210d15f717179098fe6b9334c446ae7058c9
Thomas Coopman <thomas.coopman@gmail.com>
2021-08-25 15:09:21 +0200

    refactor: change Camera to a Record

--- csharp/people-counter/Domain/Camera.cs
+++ csharp/people-counter/Domain/Camera.cs
@@ -2,17 +2,8 @@ using System;
 
 namespace Domain
 {
-    public class Camera
+    public record Camera(string Name, string Ip)
     {
-        private readonly string _name;
-        public readonly string Ip;
-
-        public Camera(string name, string ip)
-        {
-            _name = name;
-            Ip = ip;
-        }
-
         public static PeopleFlow PeopleFlow(HttpResponse response)
         {
             return new PeopleFlow(response.InAmount, response.OutAmount);
